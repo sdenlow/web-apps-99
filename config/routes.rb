a771e8e55f35bc11d9da31e42cc49c10997e8c2d
sdenlow <sydney.denlow@kellogg.northwestern.edu>
2026-02-18 01:05:38 +0000

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
 get("/tacos", :controller => "tacos", :action => "index")
+  # resources "tacos"
 get("/dice", :controller => "dice", :action => "index")
+  # resources "dice"
 get("/companies", :controller => "companies", :action => "index")
+  # resources "companies"
+get("/contacts", :controller => "contacts", :action => "index")
 end
